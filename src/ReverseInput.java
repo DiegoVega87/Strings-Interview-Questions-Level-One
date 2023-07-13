d4c11c47959761b66f000eaf0826ccee6874f2f5
Diego Vega <diegovega@me.com>
2023-07-13 13:19:29 +0200

    Add problem explanation

---
+++ src/ReverseInput.java
@@ -0,0 +1,17 @@
+public class ReverseInput {
+
+    /*
+    * For a new game, we need to implement a function that reverses a string or an integer.
+    * Can you achieve this without using extra memory (in-place)?
+    *
+    * Example 1:
+    *   arr = {'h', 'e', 'l', 'l', 'o'};
+    *   output: [o, l, l, e, h]
+    *
+    * Example 2:
+    *   arr = {'1', '2', '3', '4', '5'};
+    *   output: [5, 4, 3, 2, 1]
+    *
+    *
+     * */
+}
