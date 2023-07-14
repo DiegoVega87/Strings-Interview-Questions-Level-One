9a4fbbfa7f7a0893f9544d2652be4fb5c589e50d
Diego Vega <diegovega@me.com>
2023-07-14 13:11:44 +0200

    Add examples

--- src/NonRepeatingCharacter.java
+++ src/NonRepeatingCharacter.java
@@ -4,7 +4,20 @@ public class NonRepeatingCharacter {
     * Our startup is building a new messaging feature in our app, and we need to find the first non-repeating
     * character in a string to ensure all the messages are valid. We need a function that takes a string
     * and returns the index of the first non-repeating character.
+    *
+    * Example 1:
+    *   String s = ""leetcode"";
+    *   expectedOutput = 0;
+    *   output should be 0 since 'l' is the first non-repeating character in the string
+    *
+    * Example 2:
+    *   String s = ""startup"";
+    *   expectedOutput = 2;
+    *   output should be 2 since 'a' is the first non-repeating character in the string
+    *
     * */
+
+
 
 
 }
