1f42b9ef47ec2de9ad22621f23df80d3f5e0617e
Diego Vega <diegovega@me.com>
2023-07-14 16:10:42 +0200

    Add problem explanation

---
+++ src/PalindromeChecker.java
@@ -0,0 +1,13 @@
+public class PalindromeChecker {
+
+    /*
+     * Your startup is building a social media platform where users can share their favorite quotes.
+     * You want to ensure that the quotes shared are valid palindromes. Write a function that
+     * takes in a string and returns true if it is a palindrome, or false otherwise.
+     * ou should ignore all non-alphanumeric characters. A palindrome
+     * is a word or phrase that reads the same forward and backward, e.g. level.
+     *
+     *
+     */
+
+}
