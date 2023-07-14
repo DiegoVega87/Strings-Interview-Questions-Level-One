cc139072568d6b848344c0a36d16690dc636b180
Diego Vega <diegovega@me.com>
2023-07-14 13:50:37 +0200

    Add the problem explanation

---
+++ src/AnagramChecker.java
@@ -0,0 +1,12 @@
+public class AnagramChecker {
+
+    /*
+    * We want to implement a feature that checks if our users' passwords are anagrams to increase security.
+    * Your task is to write a function that takes in two string inputs and returns true if they form an anagram,
+    * and false if they do not. An anagram is a word or phrase formed by rearranging the letters of another word
+    * or phrase. The resulting word or phrase contains all the original letters exactly once. For example,
+    * the word ""listen"" can be rearranged to form the word ""silent,"" making ""silent"" an anagram of ""listen.""
+    *
+    *
+     * */
+}
