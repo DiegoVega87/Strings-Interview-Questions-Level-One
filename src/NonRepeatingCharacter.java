708076729746b8a74a891ad4e838a1508e470a06
Diego Vega <diegovega@me.com>
2023-07-14 13:08:32 +0200

    Add problem explanation

---
+++ src/NonRepeatingCharacter.java
@@ -0,0 +1,10 @@
+public class NonRepeatingCharacter {
+
+    /*
+    * Our startup is building a new messaging feature in our app, and we need to find the first non-repeating
+    * character in a string to ensure all the messages are valid. We need a function that takes a string
+    * and returns the index of the first non-repeating character.
+    * */
+
+
+}
