7063449c6ce60f8f7934a20927113a38980b97fb
Diego Vega <diegovega@me.com>
2023-07-14 16:30:24 +0200

    Add problem explanation

---
+++ src/StringSearch.java
@@ -0,0 +1,12 @@
+public class StringSearch {
+
+    /*
+    * String Search Function
+    * We are developing a search engine for a new product. We need to implement a function that can search
+    * for a specific string within a larger string and return the index of its first occurrence. This function
+    * will take in two strings as parameters, s1 and s2. If s1 is found within s2, the function should return the
+    * index of the first occurrence of the substring. Else return -1
+    *
+    *
+    * */
+}
