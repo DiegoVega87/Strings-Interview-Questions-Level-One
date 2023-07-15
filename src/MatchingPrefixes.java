3f7b52729f0679f143f8d0d679553d58189471e5
Diego Vega <diegovega@me.com>
2023-07-15 15:05:21 +0200

    Add problem explanation

---
+++ src/MatchingPrefixes.java
@@ -0,0 +1,10 @@
+public class MatchingPrefixes {
+
+    /*
+    * You have a database of user inputted strings to be processed and analyzed
+    * You need to find a way to efficiently determine the longest common prefix among all the strings in the database.
+    * Write a function that takes in an array of strings and returns the longest common prefix string.
+    * If none is found return """"
+    *
+    * */
+}
